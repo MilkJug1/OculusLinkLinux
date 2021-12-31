2b28c00424ff231e3fd4fcd6e8f4025d31657b45
MilkJug1 <michael.sagan.crawford@gmail.com>
2021-12-30 19:52:18 -0500

    Fixed some things and added one thing

---
+++ target/debug/deps/tracker-e3f8415655b21474.d
@@ -0,0 +1,5 @@
+/home/milk/Programming/OculusLinkLinux/target/debug/deps/tracker-e3f8415655b21474: oll/tracker/src/lib.rs
+
+/home/milk/Programming/OculusLinkLinux/target/debug/deps/tracker-e3f8415655b21474.d: oll/tracker/src/lib.rs
+
+oll/tracker/src/lib.rs:
---
+++ target/debug/deps/vrserver-400196e56712c93f.d
@@ -0,0 +1,5 @@
+/home/milk/Programming/OculusLinkLinux/target/debug/deps/vrserver-400196e56712c93f: oll/vrserver/src/main.rs
+
+/home/milk/Programming/OculusLinkLinux/target/debug/deps/vrserver-400196e56712c93f.d: oll/vrserver/src/main.rs
+
+oll/vrserver/src/main.rs:
