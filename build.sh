fb58ef1d9f9d3b608200466a4b12e0672f0765d3
MilkJug1 <michael.sagan.crawford@gmail.com>
2022-03-20 15:51:30 -0400

    made it so that it forcefully builds

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # This is only run when building in order to make it easier to build without having to go into the oll dir
 
 cd build 
-conan install ..
+conan install .. --build
 cd ..
 make 
 ./build/oll
